7d3edc06c6c7ae070b028fb9f33b4c5d49e1924b
MountainBurger <robin.sachdeva@gmail.com>
2025-10-24 23:25:19 +0100

    feat: add mapping for write with <leader>w

--- init.lua
+++ init.lua
@@ -205,6 +205,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Write quickly
+vim.keymap.set('n', '<leader>w', ':w<CR>', { noremap = true })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
